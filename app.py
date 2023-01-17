d5de1fcc301e9b1fd5b871f96b9527dc842ce0de
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-17 20:31:47 +0700

    EDIT TO HOST MOGENIUS

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
    return 'GreyMatters_Bot'
 
-#Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
+#Ex https://MS-DZULQURNAIN:ghp_147bkkabcdefgh@github.com/MS-DZULQURNAIN/FILE-SHARING
 os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
